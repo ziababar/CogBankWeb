464163b6db9665b6e56e65e4fe0f856ad6dc11df
ziababar <zbabar@gmail.com>
2018-01-08 21:14:21 -0500

    sample quiz controller

---
+++ CogBankRecommend/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CogBankRecommend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CogBankWeb/Controllers/QuizController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TestMakerFreeWebApp.ViewModels;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CogBankWeb.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuizController : Controller
+    {
+        // GET api/quiz/latest
+        [HttpGet("Latest/{num}")]
+        public IActionResult Latest(int num = 10)
+        {
+            var sampleQuizzes = new List<QuizViewModel>();
+            // add a first sample quiz
+            sampleQuizzes.Add(new QuizViewModel()
+            {
+                Id = 1,
+                Title = "Which Shingeki No Kyojin character are you?",
+                Description = "Anime-related personality test",
+                CreatedDate = DateTime.Now,
+                LastModifiedDate = DateTime.Now
+            });
+            // add a bunch of other sample quizzes
+            for (int i = 2; i <= num; i++)
+            {
+                sampleQuizzes.Add(new QuizViewModel()
+                {
+                    Id = i,
+                    Title = String.Format("Sample Quiz {0}", i),
+                    Description = "This is a sample quiz",
+                    CreatedDate = DateTime.Now,
+                    LastModifiedDate = DateTime.Now
+                });
+
+            }
+            // output the result in JSON format
+            return new JsonResult(
+            sampleQuizzes,
+            new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented
+            });
+        }
+    }
+
+}
